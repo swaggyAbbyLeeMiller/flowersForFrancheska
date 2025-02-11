cc93dc3228b9fe6047c56bda39c21a24e318d485
alexa neff <alexanef@buffalo.edu>
2025-02-10 23:05:31 -0500

    bruh  im cooked

--- js/index.js
+++ js/index.js
@@ -1,10 +1,28 @@
-const title = document.querySelector('.title')
-const text = 'Happy Valentines Day Francheska!'.split('')
-for (let index = 0; index < text.length; index++) {
-  if (text[index] !== ' ') {
-    title.innerHTML += `<span>${text[index]}<span/>`
+const title = document.querySelector('.title');
+const text = 'Happy Valentines Day Francheska!';
+
+// Split the text into two parts: one for the first line and one for the second line
+const firstLine = 'Happy Valentines Day';
+const secondLine = 'Francheska!';
+
+// Add the first part of the text
+for (let index = 0; index < firstLine.length; index++) {
+  if (firstLine[index] !== ' ') {
+    title.innerHTML += `<span>${firstLine[index]}<span/>`;
   } else {
-    title.innerHTML += `<span style='margin-right: 20px;'><span/>`
+    title.innerHTML += `<span style='margin-right: 15px;'><span/>`;
+  }
+}
+
+// Add a line break between the two parts
+title.innerHTML += `<br />`;
+
+// Add the second part of the text
+for (let index = 0; index < secondLine.length; index++) {
+  if (secondLine[index] !== ' ') {
+    title.innerHTML += `<span>${secondLine[index]}<span/>`;
+  } else {
+    title.innerHTML += `<span style='margin-right: 15px;'><span/>`;
   }
 }
 
@@ -12,4 +30,12 @@ const textElements = document.querySelectorAll('.title span');
 textElements.forEach((element) => {
   const randomDelay = Math.random() * 3;
   element.style.animationDelay = `${randomDelay}s`;
+});
+
+const button = document.querySelector('.btn');
+button.addEventListener('click', () => {
+  const newParagraph = document.createElement('p');
+  newParagraph.innerText = "Hi Ysabel!"; // Paragraph content
+  newParagraph.classList.add('new-paragraph');
+  document.body.appendChild(newParagraph); // Append to the body
 });
--- js/main.js
+++ js/main.js
@@ -1,22 +1,26 @@
+window.onload = () => {
+  const c = setTimeout(() => {
+    document.body.classList.remove("not-loaded");
 
-onload = () => {
-    const c = setTimeout(() => {
-      document.body.classList.remove("not-loaded");
-  
-      const titles = ('Happy Valentines Day To My Pretty Girl 💓').split('')
-      const titleElement = document.getElementById('title');
-      let index = 0;
-  
-      function appendTitle() {
-        if (index < titles.length) {
-          titleElement.innerHTML += titles[index];
-          index++;
-          setTimeout(appendTitle, 300); // 1000ms delay
-        }
+    const titles = ('Happy Valentines Day To My Pretty Girl 💓').split('');
+    const titleElement = document.getElementById('title');
+    let index = 0;
+
+    function appendTitle() {
+      if (index < titles.length) {
+        titleElement.innerHTML += titles[index];
+        index++;
+        setTimeout(appendTitle, 300); // 1000ms delay
+      } else {
+        // Add a paragraph after the title is fully displayed
+        const paragraph = document.createElement('p');  // Create a new paragraph element
+        paragraph.innerText = "You mean the world to me. 💖";  // Set the paragraph text
+        document.body.appendChild(paragraph);  // Append the paragraph to the body
       }
-  
-      appendTitle();
-  
-      clearTimeout(c);
-    }, 1000);
-  };
+    }
+
+    appendTitle();
+
+    clearTimeout(c);
+  }, 1000);
+};
